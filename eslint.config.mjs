e11422c2f39e457a2b13e925eb078ba0aa8d6d56
Harry Ho <4719770+harrykhh@users.noreply.github.com>
2024-10-24 03:12:10 +0000

    fix(index.ts): added missing utils imports

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,6 +6,7 @@ import pluginReact from "eslint-plugin-react";
 
 export default [
   {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
+  {ignores: ["dist/*", "src/test/*"]},
   {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,2 @@
 export * from './components/FamilyTree'
+export * from './components/utils'
